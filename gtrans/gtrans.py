a9b496d051c1f231138ca22d450f86b083f1eb9a
R5D4 <dingrand@cse.yorku.ca>
2015-09-01 11:21:12 -0400

    Update .gitignore for .swp files. Implement basic argument parser in gtrans.py.

--- gtrans/gtrans.py
+++ gtrans/gtrans.py
@@ -5,7 +5,15 @@ Usage:
     python gtrans ru en -i inputDir -o outputDir
 """
 
+import argparse
 import goslate
 
 if __name__ == "__main__":
-        
+    parser = argparse.ArgumentParser("Translate a batch of files.")
+    parser.add_argument("from_lang", help="input language")
+    parser.add_argument("to_lang", help="output language")
+    parser.add_argument("-i", help="input directory")
+    parser.add_argument("-o", help="output directory")
+    parser.add_argument("-v", "--verbosity", action="count", help="verbosity")
+    args = parser.parse_args()
+    print args
